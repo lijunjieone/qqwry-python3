8acce372da01cd3e584b339f8c84817dd1a3d803
animalize <animalize81@gmail.com>
2015-12-14 07:53:35 +0800

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(path):
 
 setup(
     name='qqwry-py3',
-    version='1.0.1',
+    version='1.0.2',
     description='Lookup address of IP in qqwry.dat, for Python 3.0+',
     long_description=read_file('qqwry.rst'),
     author='animalize',
