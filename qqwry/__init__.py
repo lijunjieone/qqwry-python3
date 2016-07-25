e445aa986384643eba3233f75f333d338a1f6096
animalize <animalize81@gmail.com>
2016-07-25 13:50:05 +0800

    版本号

--- qqwry/__init__.py
+++ qqwry/__init__.py
@@ -1,6 +1,8 @@
 from .qqwry import QQwry
 from .cz88update import updateQQwry
 
-__version__ = '1.0.6'
+__version__ = '1.0.7'
+
+__all__ = ('QQwry', 'updateQQwry')
 
 __doc__ = 'Document of this module: https://pypi.python.org/pypi/qqwry-py3'
